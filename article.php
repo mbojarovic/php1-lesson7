8a4c9401da69c4fdcdce0fecc62c5325e5dde857
Marek <m.bojarovic@gmail.com>
2016-12-24 19:05:44 +0200

    Исправлен controller article

--- article.php
+++ article.php
@@ -3,14 +3,8 @@
 require __DIR__ . '/classes/News.php';
 require __DIR__ . '/classes/View.php';
 
-if (isset($_GET['id'])) {
-    $id = (int)$_GET['id'];
-} else {
-    $id = null;
-}
-
 $news_one = new News(__DIR__ . '/data/it_news.php');
-//var_dump($news_one);
+
 $view_one = new View();
-$view_one->set('article_one', $news_one->getOneArticles($id));
+$view_one->set('article_one', $news_one->getOneArticles());
 $view_one->display(__DIR__ . '/template/article.php');
